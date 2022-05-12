00f02fdd205c7abebdc8f2dd8b06cb2c8c2d4ace
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2022-05-12 16:31:29 -0400

    gives both AA and Aa line from Gompert and Buerkle 2009 Figure 1

--- genomic.cline.plot.R
+++ genomic.cline.plot.R
@@ -1,14 +1,17 @@
 ###this is the first plot from composite.clines implemented in Introgress
-### I either need to get Josh's help to make these plots a lot prettier, or I need to translate this all to ggplot2
+### I either need to get Josh's help to make these
 genomic.cline.plot<-function(cline.data){
-  plot(0:1, 0:1, type = "n", xlab = "Hybrid index", ylab = 'Probability of Ancestry',cex.lab=2 )
+  plot(0:1, 0:1, type = "n", xlab = "Hybrid index", ylab = 'Probability of Ancestry',cex.lab=2, main=main_label)
   n.loci <- dim(cline.data$Loci.data)[1]
   hi <- cline.data$hybrid.index
   col<-c("red", "black", "black") ###hard coded for now, but could change depending on how many snps
   for (i in 1:n.loci) {
-    
     AA.line <- cline.data$Fitted.AA[i, ]
     line.matrix <- rbind(hi, AA.line)[, order(hi)]
   lines(line.matrix[1, ], line.matrix[2, ], lty = 1, col=col[i])
+  Aa.line <- cline.data$Fitted.Aa[i, ]
+  line.matrix <- rbind(hi, Aa.line)[, order(hi)]
+  lines(line.matrix[1, ], line.matrix[2, ], lty = 1, col=col[i])
+  
   }
 }
