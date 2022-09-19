f5a1e7532c8a2713490ccd4fa646ad7fbefe016b
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2022-09-19 15:59:38 -0400

    adding R2 to the anova tables

--- Remarkable_variation_submission/Table_S3_q_Q_junction_Anova.R
+++ Remarkable_variation_submission/Table_S3_q_Q_junction_Anova.R
@@ -4,81 +4,60 @@ require('stringr')
 
 library(stringr)
 library(MASS)
-#library(fitdistrplus)
-##if using local computer
-# datafiles<-list.files("~/Google Drive/Replicate Hybrid zone review/Predicting_Hybrids_analysis/hybrid_sims/deme11_all" , pattern="*first8cols.txt.gz")
-##if using teton
-datafiles<-list.files("/gscratch/buerkle/data/incompatible/runs/deme11",  pattern="*main")
-m<-str_extract(datafiles, "(\\d+\\.*\\d*)")
-c<-str_match(datafiles,"c(\\d+\\.*\\d*)")[,2]
-c[is.na(c)]<-0 #### I think this is right, as c is the measure of selection?
-mech<-str_extract(datafiles, "^([^_]+_){1}([^_])") ### where e means there's an environmental interaction and m means there isn't
 
-alldata<-list()
+source("SNP_inputs.R")
 
-# setwd("/gscratch/buerkle/data/incompatible/runs/deme11")
-
-for(i in 1:length(datafiles)){
-  alldata[[i]]<-read.table(gzfile(datafiles[i]), sep=",", header=T)
-  alldata[[i]]$m<-as.numeric(rep(m[i], nrow(alldata[[i]]))) # just giving all individuals in the sim the same m and c
-  alldata[[i]]$c<-as.numeric(rep(c[i], nrow(alldata[[i]])))
-  alldata[[i]]$mech<-as.factor(rep(mech[i], nrow(alldata[[i]])))
-}
-
-setwd("/gscratch/emcfar2/predicting_hybrids")
-alldata_df<-do.call(rbind.data.frame, alldata)
-summary(alldata_df)
-### I'm not at all sure that I need this, so maybe take it out if it's not needed. 
-alldata_df$gen<-as.factor(alldata_df$gen)
-#need to take the mean # of junctions for 19 replicates, for each generation, for each deme, for each m, for each c, and ask how the individual number of junctions for the other replicate fits the distribution
-#do this for each replicate, through all 20
 nrow<-10*length(datafiles)
 
-Fstats<-vector(length = length(unique(alldata_df_6$index)))
-pvalues<-list(length(unique(alldata_df_6$index)))
+Fstats<-vector(length = length(unique(data_deme_6$index)))
+pvalues<-list(length(unique(data_deme_6$index)))
 plots<-list()
-#only going to use middle, hybridizing demes 5 and 6
-#two reasons for this - 1), this is really what we're most interested in, and 2) we were getting -INFs when the mean njuncts was 0. 
-alldata_df_6<-subset(alldata_df, deme %in% c(6))
-alldata_df_6$index<-paste(alldata_df_6$m, alldata_df_6$c, alldata_df_6$gen, alldata_df_6$deme)
 
-Fstats_njunct<-numeric(length = length(unique(alldata_df_6$index)))
-pvalues_njunct<-numeric(length(unique(alldata_df_6$index)))
+data_deme_6$index<-paste(data_deme_6$m, data_deme_6$c, data_deme_6$mech, data_deme_6$gen)
 
-for(i in 1:length(unique(alldata_df_6$index))){
-  Fstats_njunct[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$njunct~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[7]
-  pvalues_njunct[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$njunct~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[9]
+Fstats_njunct<-numeric(length = length(unique(data_deme_6$index)))
+pvalues_njunct<-numeric(length(unique(data_deme_6$index)))
+R2_njunction<-numeric(length(unique(data_deme_6$index)))
+
+for(i in 1:length(unique(data_deme_6$index))){
+  Fstats_njunct[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$njunct~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[7]
+  pvalues_njunct[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$njunct~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[9]
+  R2_njunction[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$njunct~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]/(unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$njunct~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]+unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$njunct~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[4])
 }
 
-njunct_table<-data.frame(model=as.character(unique(alldata_df_6$index)),
-                         Fstats=Fstats_njunct, pvalues=pvalues_njunct)
+njunct_table<-data.frame(model=as.character(unique(data_deme_6$index)),
+                         Fstats=Fstats_njunct, pvalues=pvalues_njunct, Rsq=R2_njunction)
 
 #####q score####
-Fstats_q<-numeric(length = length(unique(alldata_df_6$index)))
-pvalues_q<-numeric(length(unique(alldata_df_6$index)))
+Fstats_q<-numeric(length = length(unique(data_deme_6$index)))
+pvalues_q<-numeric(length(unique(data_deme_6$index)))
+R2_q<-numeric(length(unique(data_deme_6$index)))
 
-for(i in 1:length(unique(alldata_df_6$index))){
-  Fstats_q[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$q~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[7]
-  pvalues_q[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$q~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[9]
+for(i in 1:length(unique(data_deme_6$index))){
+  Fstats_q[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$q~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[7]
+  pvalues_q[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$q~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[9]
+  R2_q[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$q~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]/(unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$q~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]+unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$q~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[4])
 }
 
-q_table<-data.frame(model=as.character(unique(alldata_df_6$index)),
-                    Fstats=Fstats_q, pvalues=pvalues_q)
+q_table<-data.frame(model=as.character(unique(data_deme_6$index)),
+                    Fstats=Fstats_q, pvalues=pvalues_q, Rsq=R2_q)
 
 
 #### het (big Q)####
 
-Fstats_het<-numeric(length = length(unique(alldata_df_6$index)))
-pvalues_het<-numeric(length(unique(alldata_df_6$index)))
+Fstats_het<-numeric(length = length(unique(data_deme_6$index)))
+pvalues_het<-numeric(length(unique(data_deme_6$index)))
+R2_het<-numeric(length(unique(data_deme_6$index)))
 
-for(i in 1:length(unique(alldata_df_6$index))){
-  Fstats_het[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$het~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[7]
-  pvalues_het[[i]]<-unlist(summary(aov(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$het~as.factor(alldata_df_6[which(alldata_df_6$index==unique(alldata_df_6$index)[i]),]$rep))))[9]
+for(i in 1:length(unique(data_deme_6$index))){
+  Fstats_het[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$het~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[7]
+  pvalues_het[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$het~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[9]
+  R2_het[[i]]<-unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$het~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]/(unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$het~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[3]+unlist(summary(aov(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$het~as.factor(data_deme_6[which(data_deme_6$index==unique(data_deme_6$index)[i]),]$rep))))[4])
 }
 
-het_table<-data.frame(model=as.character(unique(alldata_df_6$index)), Fstats=Fstats_het, pvalues=pvalues_het)
+het_table<-data.frame(model=as.character(unique(data_deme_6$index)), Fstats=Fstats_het, pvalues=pvalues_het,  Rsq=R2_het)
 
-save.image("/gscratch/emcfarl2/predicting_hybrids/Fstats_individuals.RData")
+#save.image("/gscratch/emcfarl2/predicting_hybrids/Fstats_individuals.RData")
 
 design.table<-data.frame(model=het_table$model,
                          migration=numeric(nrow(het_table)),
