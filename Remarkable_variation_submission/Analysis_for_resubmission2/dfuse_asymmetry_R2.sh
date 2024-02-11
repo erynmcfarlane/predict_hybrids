7a6a60b1a9d3288711d41048e5a755f5ab3bc2b8
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2024-02-11 16:28:13 -0500

    cleaning up archive and R2 changes

--- Remarkable_variation_submission/Analysis_for_resubmission2/dfuse_asymmetry_R2.sh
+++ Remarkable_variation_submission/Analysis_for_resubmission2/dfuse_asymmetry_R2.sh
@@ -53,3 +53,40 @@ module load r
 ./dfuse -d 3_deme_uneven_10_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.2 -c 0 -o ./tenone/path_m0.2_neutral
 ./dfuse -d 3_deme_uneven_10_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.2 -o ./tenone/path_m0.2_c0.2
 ./dfuse -d 3_deme_uneven_10_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.9 -o ./tenone/path_m0.2_c0.9
+
+
+
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.01 -c 0 -o ./fiveone/dmi_m0.01_neutral
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.2 -o ./fiveone/dmi_m0.01_c0.2
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.9 -o ./fiveone/dmi_m0.01_c0.9
+
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.2 -c 0 -o ./fiveone/dmi_m0.2_neutral
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.2 -o ./fiveone/dmi_m0.2_c0.2
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.9 -o ./fiveone/dmi_m0.2_c0.9
+
+###path
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.01 -c 0 -o ./fiveone/path_m0.01_neutral
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.2 -o ./fiveone/path_m0.01_c0.2
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.9 -o ./fiveone/path_m0.01_c0.9
+
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.2 -c 0 -o ./fiveone/path_m0.2_neutral
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.2 -o ./fiveone/path_m0.2_c0.2
+./dfuse -d 3_deme_uneven_5_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.9 -o ./fiveone/path_m0.2_c0.9
+
+
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.01 -c 0 -o ./twoone/dmi_m0.01_neutral
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.2 -o ./twoone/dmi_m0.01_c0.2
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.9 -o ./twoone/dmi_m0.01_c0.9
+
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.2 -c 0 -o ./twoone/dmi_m0.2_neutral
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.2 -o ./twoone/dmi_m0.2_c0.2
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_1.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.9 -o ./twoone/dmi_m0.2_c0.9
+
+###path
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.01 -c 0 -o ./twoone/path_m0.01_neutral
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.2 -o ./twoone/path_m0.01_c0.2
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.01 -c 0.9 -o ./twoone/path_m0.01_c0.9
+
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1_neutral.txt -r 20 -g 100 -m 0.2 -c 0 -o ./twoone/path_m0.2_neutral
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.2 -o ./twoone/path_m0.2_c0.2
+./dfuse -d 3_deme_uneven_2_1.txt -e /gscratch/buerkle/data/incompatible/runs/inputfiles/epifile_2c.txt -s /gscratch/buerkle/data/incompatible/runs/inputfiles/selfile_1.txt -r 20 -g 100 -m 0.2 -c 0.9 -o ./twoone/path_m0.2_c0.9
