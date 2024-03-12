959a7e37cbe013fc4015b87264c1abdc8dcae853
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2024-03-12 12:43:08 -0400

    pulling table S1 for 2:1, 5:1, 10:1 and 50:1

--- Remarkable_variation_submission/Analysis_for_resubmission2/Table_S3_q_Q_junction_Anova_unevenparents.R
+++ Remarkable_variation_submission/Analysis_for_resubmission2/Table_S3_q_Q_junction_Anova_unevenparents.R
@@ -25,8 +25,10 @@ for(i in 1:length(unique(data_deme_2$index))){
   R2_njunction[[i]]<-unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$njunct~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[3]/(unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$njunct~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[3]+unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$njunct~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[4])
 }
 
-njunct_table<-data.frame(model=as.character(unique(data_deme_2$index)),
-                         Fstats=Fstats_njunct, pvalues=pvalues_njunct, Rsq=R2_njunction)
+njunct_table<-data.frame(model=as.character(unique(data_deme_2$index)),Fstats=Fstats_njunct, pvalues=pvalues_njunct, Rsq=R2_njunction)
+
+
+
 
 #####q score####
 Fstats_q<-numeric(length = length(unique(data_deme_2$index)))
@@ -39,8 +41,7 @@ for(i in 1:length(unique(data_deme_2$index))){
   R2_q[[i]]<-unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$q~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[3]/(unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$q~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[3]+unlist(summary(aov(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$q~as.factor(data_deme_2[which(data_deme_2$index==unique(data_deme_2$index)[i]),]$rep))))[4])
 }
 
-q_table<-data.frame(model=as.character(unique(data_deme_2$index)),
-                    Fstats=Fstats_q, pvalues=pvalues_q, Rsq=R2_q)
+q_table<-data.frame(model=as.character(unique(data_deme_2$index)),Fstats=Fstats_q, pvalues=pvalues_q, Rsq=R2_q)
 
 
 #### het (big Q)####
@@ -59,10 +60,21 @@ het_table<-data.frame(model=as.character(unique(data_deme_2$index)), Fstats=Fsta
 
 #save.image("/gscratch/emcfarl2/predicting_hybrids/Fstats_individuals.RData")
 
+cbind(q_table, het_table[,2:4],njunct_table[,2:4])->everything_table
+str_extract(everything_table$model, stringr::regex("(\\d+)(?!.*\\d)"))->everything_table$gen
+everything_table[which(everything_table$gen==10),]->gen10
+everything_table[which(everything_table$gen==100),]->gen100
+rbind.data.frame(gen10, gen100)->gen10_100
+
+write.csv(gen10_100, file="anovatable_twoone.csv", row.names = FALSE)
+
 design.table<-data.frame(model=het_table$model,
                          migration=numeric(nrow(het_table)),
                          selection=numeric(nrow(het_table)),
                          generation=numeric(nrow(het_table)))
+
+design.table[which(design.table$gen==10)]
+
 
 tmp<-matrix(as.numeric(unlist(strsplit(design.table$model, split=" "))),
             ncol=4, byrow=TRUE)
